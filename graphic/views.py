e0fea26cc09cd1e9d73183c1ecf54dbd105174a5
suelliton <tonmelodicmetal@gmailcom>
2016-05-21 00:26:03 -0300

    commit automatico

--- graphic/views.py
+++ graphic/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 # Funcao imaginaria para manipular um upload de arquivo.
 from django.core.files.uploadedfile import UploadedFile
-
+"""
 def index(request):
     form = FormArquivo(request.POST, request.FILES)
     if request.method == "POST":
@@ -22,9 +22,12 @@ def index(request):
 
 
 
-
 def grafico(arquivo):
 #aqui tenho que receber o arquivo de texto em estado bruto
+
+
+
+
 
     x =[]
     a = 0
@@ -52,3 +55,44 @@ def grafico(arquivo):
 		    e +=1
     dados = [a,b,c,d,e]
     return render_to_response('grafico/grafico.html',{})
+"""
+
+def  index ( request ): 
+    if request.method  ==  'POST' : 
+        form  =  FormArquivo( request.POST , request.FILES ) 
+        if  form.is_valid(): 
+            grafico(request.FILES[ 'arq' ].read) 
+            return render_to_response('grafico/grafico.html/',{} ) 
+    else : 
+        form = FormArquivo() 
+    return  render ( request ,'grafico/index.html' ,  { 'form' :  form })
+
+
+
+
+def grafico(f):
+    x =[]
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    e = 0
+
+    with open('arq', 'wb+') as destination:
+        reader=csv.reader(destination, delimiter = ' ')
+        for row in reader:
+            x.append(float(row[0]))
+
+    for i in range(0,len(x)-1):
+        if(x[i] >10.0  and x[i] <= 12.2):
+            a +=1
+        elif( x[i] <= 14.4):
+            b +=1
+        elif( x[i] <= 16.6):
+            c +=1
+        elif( x[i] <= 18.8):
+            d +=1
+        else:
+            e +=1
+    dados = [a,b,c,d,e]
+    return render_to_response('grafico/grafico.html/',{'dados':dados} ) 
